739b3f1f85b5a032ea3cfdcdfc43a883f41260ef
Siddhant <siddhantgk01@gmail.com>
2023-02-08 21:39:25 +0530

    First commit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
+import java.util.*;
 
 public class RWOnly {
-    
+     String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
